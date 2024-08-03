69760d7b1d0721a4d8b76ec566988c5eaca78daa
Jose D. Romero <josedanilromero@gmail.com>
2024-08-02 20:21:22 -0500

    feat(GamePanel): Add methods and create MyKeyAdapter inner class (read description for details).
    This commits the following methods - startGame(), paintComponent(), draw(), move(), checkApple(), checkCollisions(), gameOver(), actionPerformed().
    
    The class added is MyKeyAdapter, which extends the KeyAdapter class.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,53 @@
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.JPanel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
 
-    GamePanel(){
-        
+    GamePanel() {
+
     }
+
+    public void startGame() {
+
+    }
+    
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         // Fill in Method here
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
