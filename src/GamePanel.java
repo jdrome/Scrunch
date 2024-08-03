20966c980da797a10fc7cd8dd2ba3e979777b54a
Jose D. Romero <josedanilromero@gmail.com>
2024-08-02 19:59:34 -0500

    feat(GamePanel): Implement ActionListener Interface
    I needed to add a couple more imports to the GamePanel file since it needs to import Java AWT's ActionListener interface

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,11 @@
 import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class GamePanel extends JPanel {
-    
+public class GamePanel extends JPanel implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Fill in Method here
+    }
 }
