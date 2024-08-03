2f479b293e5ab6a832e344bb0c7e38b754086271
Jose D. Romero <josedanilromero@gmail.com>
2024-08-03 18:32:25 -0500

    feat(GamePanel): Add attributes to GamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -10,6 +10,17 @@ public class GamePanel extends JPanel implements ActionListener{
     static final int screenHeight = 600;
     static final int unitSize = 25; // Size of items in the game.
     static final int gameUnits = (screenWidth * screenHeight / unitSize); // Calculates how many items we can have in our game.
+    static final int delay = 75; // The higher the number, the slower the game.
+    final int x[] = new int[gameUnits]; // X coordinates (including head) of snake's body parts.
+    final int y[] = new int [gameUnits]; // Y coordinates (including head) of snake's body parts.
+    int bodyParts = 6; // Initial length of the snake (i.e. how many "body parts" you start with).
+    int applesEaten; // This is the count of apples eaten. Initialized at 0.
+    int appleX; // Random X coordinate of the apple that the snake eats. Becomes random every time the snake eats an apple.
+    int appley; // Random Y coordinate of the apple that the snake eats. Becomes random every time the snake eats an apple.
+    char direction = 'R'; // Snake begins by going right. R = right; L = left; U = up; D = down.
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     GamePanel() {
 
